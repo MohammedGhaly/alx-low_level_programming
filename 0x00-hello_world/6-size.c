d667d365f231090cad4c77b3c4dbfdb7c77d1096
MohammedGhaly <medooghaly7@gmail.com>
2023-02-16 14:31:00 -0800

    a new commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d byte(s)", (int)sizeof(a));
+	printf("Size of a int: %d byte(s)", (int)sizeof(b));
+	printf("Size of a long int: %d byte(s)", (int)sizeof(c));
+	printf("Size of a long lkong int: %d byte(s)", (int)sizeof(d));
+	printf("Size of a float: %d byte(s)", (int)sizeof(e));
+	reutrn (0);
+}
+
