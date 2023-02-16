a6fd6c2cce91e0d8dde2e4b09d2b99b0d6fce1c0
MohammedGhaly <medooghaly7@gmail.com>
2023-02-16 14:12:14 -0800

    a new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void){
+/*
+ * main		entry point
+ * print the required text
+ * return 0 	(success)
+ *
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
