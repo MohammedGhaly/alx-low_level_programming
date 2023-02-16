eff24773412e2bc4bb9f8f545bfc060bd7f9af22
MohammedGhaly <medooghaly7@gmail.com>
2023-02-16 14:14:25 -0800

    a new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main		entry point
- * print the required text
- * return 0 	(success)
+
+/**
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
