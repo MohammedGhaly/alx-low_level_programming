b755c6235750a1f3c6e3240600ee302a9931ca50
MohammedGhaly <medooghaly7@gmail.com>
2023-02-17 07:20:04 -0800

    a new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
