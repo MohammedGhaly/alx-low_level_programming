77e47d6602aaa8117d0b02890a907b2b8fea8060
MohammedGhaly <medooghaly7@gmail.com>
2023-02-17 05:16:30 -0800

    a new commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i = 97;
 	int j = 65;
-	
+
 	while (i < 123)
 	{
 		putchar(i);
 		i++;
 	}
 	while (j < 91)
-       	{
+	{
 		putchar(j);
-	        j++;
+		j++;
 	}
 	putchar('\n');
 	return (0);
