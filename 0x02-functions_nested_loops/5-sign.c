84357724734f6444e2dfd0c6bf3fbac53c11a778
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 09:50:12 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase or uppercase character
+ *
+ *@n: integer number
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	}
+	else
+	{
+		putchar(45);
+		return (-1);
+	}
+}
