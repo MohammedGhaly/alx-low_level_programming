7e272a07d21e719c6e714557f8e3b3011f85177d
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 10:01:43 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - checks for lowercase or uppercase character
+ *
+ *@n: integer number
+ *
+ * Return: n if n is non-negative integer, -n if n is negative integer (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
