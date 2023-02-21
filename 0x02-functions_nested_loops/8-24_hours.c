e9dcc1195ea1d25c249a56e40100a61115e910e3
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 15:50:13 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: the last digit of the integer Always (Success)
+ */
+void jack_bauer(void){
+	int m = 0;
+	int h = 0;
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h - (h % 10)) / 10 + 48);
+			_putchar((h % 10) + 48);
+			-putchar(58);
+			_putchar((m - (m % 10)) / 10 + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+			m++;
+		}
+		m = 0;
+		h++;
+	}
+}
