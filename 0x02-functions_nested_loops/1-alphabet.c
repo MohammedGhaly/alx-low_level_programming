29207b69b6870549c891646a856ca9b876b0110d
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 07:14:15 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i <= 122 )
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
