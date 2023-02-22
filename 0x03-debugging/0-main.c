4fd5ff35a1d0c4b808724122df1157df9906311f
MohammedGhaly <medooghaly7@gmail.com>
2023-02-22 11:01:22 -0800

    a new commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,13 @@
  *
  * main - tests the sign of an integer
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
-
