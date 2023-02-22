7b8ab91a2abeedc24aee7e0e103494473aa6e671
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 16:29:46 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table from 1 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int f = 0;
+	int s = 9;
+	int r = 0;
+
+	while (f < 10)
+	{
+		while (s < 10)
+		{
+			r = f*s;
+			if (r > 9)
+			{
+				_putchar(((r - (r % 10)) / 10 + 48));
+				_putchar((r % 10) + 48);
+				_putchar(44);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(48 + r);
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			s++;
+		}
+		_putchar('\n');
+		s = 0;
+		f++;
+	}
+}
