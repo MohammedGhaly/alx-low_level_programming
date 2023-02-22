3c7f74cdcd507a64534992966391747a467ec389
MohammedGhaly <medooghaly7@gmail.com>
2023-02-21 17:04:30 -0800

    a new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase or uppercase character
+ *
+ *@x: integer 1
+ *
+ *@y: integer 2
+ *
+ * Return: the sum of x and y (Success)
+ */
+int add(int x, int y){
+	return (x + y);
+}
