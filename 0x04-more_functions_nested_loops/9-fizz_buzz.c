9d2fe5398c7ed45f0f1b085bab68e452a868755a
MohammedGhaly <medooghaly7@gmail.com>
2023-02-23 11:15:08 -0800

    a new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,22 @@
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-		{
-			printf("FizzBuzz ");
-		}
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
 		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
+			printf("%s ", f);
 		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+			printf("%s ", b);
 		else
-		{
 			printf("%d ", i);
-		}
 	}
 	printf("\n");
 	return (0);
