2cbcd211072f00dbc0bc1003ccff118fea6dcb44
MohammedGhaly <medooghaly7@gmail.com>
2023-02-23 10:14:39 -0800

    a new commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,23 +3,23 @@
 /**
  * print_square - a function that prints a square
  *
- *@n: size of the square
+ *@size: size of the square
  *
  * Return: 0 Always success
  */
-void print_square(int n)
+void print_square(int size)
 {
 	int i;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < size; j++)
 		{
 			_putchar(35);
 		}
