09a1f6ace7723d288cd51284ade05f467bb06dda
MohammedGhaly <medooghaly7@gmail.com>
2023-02-23 07:51:53 -0800

    a new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two numbers.
+ *
+ *@a: the first digit
+ *
+ *@b: the second digit
+ *
+ * Return: the multiplication of a * b.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+}
