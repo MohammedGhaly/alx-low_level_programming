a9806b916cbe83582ed6274e6023cdc9171f4432
MohammedGhaly <medooghaly7@gmail.com>
2023-02-23 11:00:12 -0800

    a new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
- *
- * Return: 0 Always success
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,15 +14,15 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 15) == 0)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
