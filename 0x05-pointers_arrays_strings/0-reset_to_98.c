51cce84d10eb6899312d3642e61aea170595e60f
MohammedGhaly <medooghaly7@gmail.com>
2023-02-27 11:58:35 -0800

    a new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * reset_to_98 - resets the variable passed to 98
+ *
+ * @n: the address of the variable to be changed
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
