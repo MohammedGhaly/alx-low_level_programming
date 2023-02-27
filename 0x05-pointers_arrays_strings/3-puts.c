6d2b807554a8069168b1a5ebee125aee4246fefe
MohammedGhaly <medooghaly7@gmail.com>
2023-02-27 12:37:33 -0800

    a new commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  * _puts - mimics puts fun of the standard library
  *
- *@s: the pointer of the first of the string
+ *@str: the pointer of the first of the string
  *
  * Return: Always 0.
  */
