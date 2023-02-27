95fa24b272eecae62dcd0c7693b5f13f0e499ccb
MohammedGhaly <medooghaly7@gmail.com>
2023-02-27 12:23:33 -0800

    a new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *
+ *@s: the pointer of the first char of the string
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
