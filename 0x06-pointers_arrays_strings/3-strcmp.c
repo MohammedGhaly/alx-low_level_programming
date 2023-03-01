580a6860e8b79109ec9a5301bc16500ba0de4554
MohammedGhaly <medooghaly7@gmail.com>
2023-03-01 14:30:47 -0800

    a new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,17 +18,17 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (*s1 > *s2)
 		{
-			return (1);
+			return (15);
 		}
 		else
-			return (-1);
+			return (-15);
 	}
 	if (*s1 == '\0')
 	{
 		if (*s2 == '\0')
 			return (0);
 		else
-			return (-1);
+			return (-15);
 	}
-	return (1);
+	return (15);
 }
