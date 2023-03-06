1246d905d67586b25055c01786d3bfc032ca2461
MohammedGhaly <medooghaly7@gmail.com>
2023-03-06 12:59:15 -0800

    a new commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (haystack[i] != '\0')
+	{
+		int j = 0;
+		int k = i
+
+		while (neddle[j] != '\0')
+		{
+			if (haystack[k] == needle[j])
+			{
+				j++;
+				k++;
+				continue;
+			} else
+				break;
+		}
+		if (needle[j] == '\0')
+			return (s + i);
+
+		i++;
+	}
+	return (NULL);
+}
