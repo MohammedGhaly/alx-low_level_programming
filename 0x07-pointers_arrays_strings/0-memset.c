3ed24ee3ad9e6e40c74855575364611c5f5b8d51
MohammedGhaly <medooghaly7@gmail.com>
2023-03-06 11:40:12 -0800

    a new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,13 @@
  *@s: pointer to the first char
  *@b: the character that fills the memory
  *@n: the number of bytes to be filled with b
- * Return: pointer to the same place in memory
+ * Return Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+		s[i] = b;
 	return (s);
 }
