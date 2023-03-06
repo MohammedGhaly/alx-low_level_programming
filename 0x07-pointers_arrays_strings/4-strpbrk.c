ed61a9f09846056d068fedd27ff34fa7fc639d2a
MohammedGhaly <medooghaly7@gmail.com>
2023-03-06 12:36:33 -0800

    a new commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		int j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
