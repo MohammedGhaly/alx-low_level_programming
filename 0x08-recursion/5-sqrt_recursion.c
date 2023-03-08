74e1b656c578049aecd1550d8b641a24f797a973
MohammedGhaly <medooghaly7@gmail.com>
2023-03-08 05:09:54 -0800

    a new commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates the square root of n
+ * @n: base integer
+ * Return: -1 if n < 0, and the correct result otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (squareRoot(n, (n + 1) / 2));
+}
+/**
+ * squareRoot - calculates the square root of a number n
+ * @n: integer
+ * @i: iteration parameter
+ * Return: -1 if no sqrt found, and the correct result otherwise
+ */
+int squareRoot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (squareRoot(n, i - 1));
+}
