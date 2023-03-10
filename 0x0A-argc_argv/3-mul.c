f7778d5961d80b0ca7b4035bc0ff2df1cfc0c165
MohammedGhaly <medooghaly7@gmail.com>
2023-03-10 11:44:07 -0800

    a new commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main -entry point
+ *@argc: number of arguments of the program
+ *@argv: list of arguments of the program
+ * Return: 0 Always Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
