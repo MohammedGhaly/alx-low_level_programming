6a85e9ef93bf15b9b948f3dcf8544719ba371e3d
MohammedGhaly <medooghaly7@gmail.com>
2023-03-10 10:58:39 -0800

    a new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *@argc: number of arguments 
+ *@argv: array of arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
