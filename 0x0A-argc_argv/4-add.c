ca33f33cef9b14a9cc9fb99f27352426cc37c0c9
MohammedGhaly <medooghaly7@gmail.com>
2023-03-10 12:02:19 -0800

    a new commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main -entry point
+ *@argc: number of arguments of the program
+ *@argv: list of arguments of the program
+ * Return: 0 Always Success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
