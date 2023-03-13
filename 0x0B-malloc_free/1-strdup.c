abdac6acc96a6dfe0bc4f0206060835224084e40
MohammedGhaly <medooghaly7@gmail.com>
2023-03-13 15:01:30 -0700

    a new commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - creates an array of chars
+ * @str: the string that will be copies
+ * Return: pointer to the new string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int strcounter = 0;
+	char *newstr = malloc(sizeof(char) * (strcounter));
+
+	if (str == NULL)
+		return NULL;
+
+	while (str[strcounter] != '\0')
+		strcounter++;
+
+	while (str[i] != '\0')
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	newstr[i] = '\0';
+
+	if (newstr == NULL)
+		return (NULL);
+
+	return newstr;
+}
