11cd94b5c70fb1e50c4b813e1aae9ab741e199ed
MohammedGhaly <medooghaly7@gmail.com>
2023-03-13 16:17:29 -0700

    a new commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - creates an grid of zeros
+ * @width: width of the matrix
+ * @height: height of the matrix
+ * Return: pointer to the array of arrays
+ */
+int **alloc_grid(int width, int height)
+{
+	int **bigArray;
+	int *smallArray;
+	int i;
+	int j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	bigArray = malloc((width * height) * sizeof(int));
+	for (i = 0; i < height; i++)
+	{
+		smallArray = malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+			smallArray[j] = 0;
+		bigArray[i] = smallArray;
+	}
+	return (bigArray);
+}
