d3ffb14364f7f60e5d5ff02880102f70dc5ed66d
MohammedGhaly <medooghaly7@gmail.com>
2023-03-13 05:47:28 -0700

    a new commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
 	unsigned int i;
 	char *ptr = malloc(size * sizeof(char));
+
+	if (size == 0)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		*(ptr + i) = c;
