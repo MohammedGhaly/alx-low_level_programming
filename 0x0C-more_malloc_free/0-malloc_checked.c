71213ab843392faecff3611c70b0392725621b08
MohammedGhaly <medooghaly7@gmail.com>
2023-03-16 05:25:26 -0700

    a new commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: number of bytes to be allocated
+ * Return: a pointer to the first byte of the allocatd memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+	return (ptr);
+}
