aa86764321231cf2815a01f27e992376d2f21e20
MohammedGhaly <medooghaly7@gmail.com>
2023-03-20 14:25:24 -0700

    a new commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,11 +32,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	copy_name = malloc(sizeof(char) * (name_len + 1));
 	if (copy_name == NULL)
+	{
+		free(new_name);
 		return (NULL);
+	}
 
 	copy_owner = malloc(sizeof(char) * (owner_len + 1));
 	if (copy_owner == NULL)
+	{
+		free(new_name);
 		return (NULL);
+	}
 
 	for (x = 0; x <= name_len; x++)
 		copy_name[x] = name[x];
