21e339fd13aa7c23f8b6cbb45e3778334dade965
MohammedGhaly <medooghaly7@gmail.com>
2023-03-20 08:38:57 -0700

    a new commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,9 @@
+tasks of 0x0E-structures_typedef
+
+concepts: 
+
+1- Structures
+2- struct (C programming language)
+3- Documentation: structures
+4- Typedef and structers
+5- typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct my_struct - contains dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: contains fields of dog name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
