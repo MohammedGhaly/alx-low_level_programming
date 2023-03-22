150152d41b6f201ed238d58e49b9175f23adffdd
MohammedGhaly <medooghaly7@gmail.com>
2023-03-22 05:10:17 -0700

    a new commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (!array || !action)
-        return;
+	if (!array || !action)
+		return;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
