acae54a5de0114626d3424867727a22696a5ad30
MohammedGhaly <medooghaly7@gmail.com>
2023-03-22 04:48:13 -0700

    a new commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name according to the function passed
+ * @name: name string
+ * @f: pointer to a function
+ * Return: Success Always
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
