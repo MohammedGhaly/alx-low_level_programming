eda02c264fb2305bc20a5f7bd9c5d8ded9616142
MohammedGhaly <medooghaly7@gmail.com>
2023-03-30 15:06:42 -0700

    a new commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+
+/**
+ * free_list - frees the memory of the list
+ * @head: head node of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t **temp;
+
+	while (head->next != NULL)
+	{
+		*temp = head;
+		head = head->next;
+		free((*temp)->str);
+		free(*temp);
+	}
+	free(head);
+}
