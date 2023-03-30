aa701f59f7ef9e41b1bfe320e119d8aedc90532f
MohammedGhaly <medooghaly7@gmail.com>
2023-03-30 15:12:29 -0700

    a new commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head->next != NULL)
+	while ((temp = head) != NULL)
 	{
-		temp = head;
 		head = head->next;
 		free(temp->str);
 		free(temp);
 	}
-	free(head);
 }
