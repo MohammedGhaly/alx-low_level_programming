be00e822a9b286e8e7d881d98781cb37b542050f
MohammedGhaly <medooghaly7@gmail.com>
2023-04-08 08:15:49 -0700

    solving task 4. 100

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the bit at index to 1
+ * @n: integer
+ * @index: integer
+ * Return: 1 for Success, -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *temp;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	temp = malloc(sizeof(unsigned long int));
+	*temp = *n;
+	if (!(*temp & (1 << index)))
+	{
+		*n = *temp + twoPowerOf(index);
+		return (1);
+	}
+	return (-1);
+}
+
+/**
+ * twoPowerOf - calculates 2 power of p
+ * @p: integer
+ * Return: the result of 2 power of p
+ */
+unsigned long int twoPowerOf(int p)
+{
+	unsigned long int res = 1;
+
+	if (p != 0)
+	{
+		for (; p > 0; p--)
+			res = res * 2;
+	}
+	return (res);
+}
