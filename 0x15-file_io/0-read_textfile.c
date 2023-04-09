2cd231a62691a9c960b3d22d901f2c177e55771e
MohammedGhaly <medooghaly7@gmail.com>
2023-04-09 16:41:25 -0700

    fixing betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,22 +8,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, n;
-    char *buffer[1024];
+	int fd, n;
+	char *buffer[1024];
 
-    if (!filename)
-       return (0);
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    n = read(fd, buffer, letters);
-    if ((write(1, buffer, n)) != n)
-    {
-        close(fd);
-        return (0);
-    }
-    close(fd);
-    return (n);
+	n = read(fd, buffer, letters);
+	if ((write(1, buffer, n)) != n)
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return (n);
 }
