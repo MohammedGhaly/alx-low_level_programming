91afa40ddd2988651a75c79a8997ea0841899038
MohammedGhaly <medooghaly7@gmail.com>
2023-04-09 16:39:09 -0700

    solving task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints its content to standard output
+ * @filename: string
+ * @letters: number of letters to be printed
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd, n;
+    char *buffer[1024];
+
+    if (!filename)
+       return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    n = read(fd, buffer, letters);
+    if ((write(1, buffer, n)) != n)
+    {
+        close(fd);
+        return (0);
+    }
+    close(fd);
+    return (n);
+}
