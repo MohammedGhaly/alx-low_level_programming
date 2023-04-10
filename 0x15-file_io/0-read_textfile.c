46513a26f0ae1eafb6b5b584c95397d60393cd8f
MohammedGhaly <medooghaly7@gmail.com>
2023-04-09 17:00:12 -0700

    dynamically allocating memory for buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, n;
-	char *buffer[1024];
+	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 	n = read(fd, buffer, letters);
 	if (n == -1)
+	{
+		free(buffer);
 		return (0);
-	n = write(1, buffer, n); 
-	if (n != -1)
+	}
+	if ((write(1, buffer, n)) != n)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	close(fd);
+	free(buffer);
 	return (n);
 }
