a7a02737da9b72841026056acba07d1512b4c9cd
MohammedGhaly <medooghaly7@gmail.com>
2023-05-18 08:15:58 -0700

    changing implementation of 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,41 +8,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node = *head;
-	dlistint_t *tmp;
-	unsigned int i = 0;
+	dlistint_t *current;
+	unsigned int counter;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
-	if (index == 0)
+
+	current = *head;
+	if (!index)
 	{
-		if (*head == NULL)
-			return (0);
-		if ((*head)->next == NULL)
-		{
-			*head = NULL;
-			return (1);
-		}
-		*head = (*head)->next;
-		(*head)->prev = NULL;
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+		free(current);
 		return (1);
 	}
-	while (i < index && node != NULL)
-	{
-		node = node->next;
-		i++;
-	}
-	if (node == NULL)
+	for (counter = 0; current != NULL && counter < index - 1 ; counter++)
+		current = current->next;
+	if (current == NULL || current->next == NULL)
 		return (-1);
 
-	tmp = node->prev;
-	if (node->next != NULL)
+	if (current->next->next != NULL)
 	{
-		tmp->next = node->next;
-		node->next->prev = tmp;
+		current->next = current->next->next;
+		free(current->next->prev);
+		current->next->prev = current;
+		return (1);
 	}
 	else
-		tmp->next = NULL;
-	free(node);
-	return (1);
+	{
+		free(current->next);
+		current->next = NULL;
+		return (1);
+	}
+	return (-1);
 }
