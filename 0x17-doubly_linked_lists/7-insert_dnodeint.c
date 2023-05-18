02a38395577ca87a6197593148588d0ebc56a6e5
MohammedGhaly <medooghaly7@gmail.com>
2023-05-18 07:28:45 -0700

    fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,6 +31,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new);
 		return (NULL);
 	}
+	if (i == (idx - 1) && cur->next == NULL)
+		return (add_dnodeint_end(h, n));
 	new->next = cur->next;
 	cur->next->prev = new;
 	cur->next = new;
