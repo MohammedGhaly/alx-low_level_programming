9d263a20afae263a990440bea94895372357a8f4
MohammedGhaly <medooghaly7@gmail.com>
2023-06-08 14:47:01 -0700

    fixing compare types err

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	hash_node_t **array;
-	int i = 0;
+	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
