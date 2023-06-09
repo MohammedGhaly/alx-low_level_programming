4246ac04f6cf079a84f0470b5dcfc12cf5848143
MohammedGhaly <medooghaly7@gmail.com>
2023-06-08 22:01:24 -0700

    fixing task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *node;
 
-	if (!ht || !key || !value || *key == '\0')
+	if (!ht || !key || !value || *key == '\0' || value == NULL)
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
